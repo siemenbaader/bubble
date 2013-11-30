6d4645ac124e5cf531a1cf7155baf7c50e262cd4
Siemen Baader <siemenbaader@gmail.com>
2013-11-30 15:25:46 +0100

    initial

---
+++ js/Bubble.js
@@ -0,0 +1,7 @@
+define("bubble/Bubble", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st){
+smalltalk.addPackage('Bubble');
+smalltalk.packages["Bubble"].transport = {"type":"amd","amdNamespace":"bubble"};
+
+smalltalk.addClass('App', smalltalk.Object, [], 'Bubble');
+
+});
